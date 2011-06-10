137a36476d692c1cd33ac7cc24dcd0fb42cb5ae9
Juha Paananen <juha.paananen@reaktor.fi>
2011-06-11 00:18:33 +0300

    dispose function works now too

--- rx.hs
+++ rx.hs
@@ -8,7 +8,7 @@ import Control.Monad
 {- Generic interfaces -}
 
 class Observable x a where
-	subscribe :: a -> Observer x -> Disposable
+	subscribe :: a -> Observer x -> IO Disposable
 
 type Observer x = (x -> IO ())
 
@@ -16,27 +16,41 @@ type Disposable = IO ()
 
 {- Sample implementation : PushCollection -}
 
-data PushCollection a = PushCollection (IORef [Observer a])
+data Subscription a = Subscription (Observer a) Int
+instance Eq (Subscription q) where
+  (==) (Subscription _ a) (Subscription _ b) = a == b 
+
+data PushCollection a = PushCollection (IORef ([Subscription a], Int))
 
 instance Observable a (PushCollection a) where
-  subscribe (PushCollection listRef) subscriber = do
-    observers <- readIORef listRef
-    writeIORef listRef $ subscriber : observers 
+  subscribe (PushCollection ref) observer = do
+    (observers, id) <- readIORef ref
+    let subscription = Subscription observer id
+    writeIORef ref $ (subscription : observers, id+1) 
+    return (removeFromListRef ref subscription)
 
-newPushCollection = liftM PushCollection (newIORef [])
+removeFromListRef ref subscriber = do
+    (observers, id) <- readIORef ref
+    writeIORef ref $ (filter (/= subscriber) observers, id)
+  
+newPushCollection = liftM PushCollection (newIORef ([], 1))
 
 push :: PushCollection a -> a -> IO ()
 push (PushCollection listRef) item = do
-    observers <- readIORef listRef
+    (observers, _) <- readIORef listRef
     mapM_  (applyTo item) observers
-  where applyTo = flip ($)
+  where applyTo item (Subscription observer _) = observer item
 
 {- "Main" for testing it -}
 
 main :: IO ()
 main = do
   pushCollection <- newPushCollection 
-  disposable <- subscribe pushCollection putStrLn
-  push pushCollection "epic"
+  dispose <- subscribe pushCollection putStrLn
+  dispose2 <- subscribe pushCollection putStrLn
+  push pushCollection "Should be printed twice"
+  dispose
+  push pushCollection "Should be printed once"
   putStrLn "done"
+
 
