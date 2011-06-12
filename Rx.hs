0d388d33e8b27204670cbebc819ec4c0c325558c
Juha Paananen <juha.paananen@gmail.com>
2011-06-12 13:19:45 +0300

    merge ObservableList into Rx

--- Rx.hs
+++ Rx.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE MultiParamTypeClasses,FlexibleInstances,TypeSynonymInstances #-}
 module Rx where
 
+import Control.Monad
+
 class Observable x a where
 	subscribe :: a -> Observer x -> IO Disposable
 
@@ -12,3 +14,8 @@ type Subscribe a = (Observer a -> IO Disposable)
 
 instance Observable x (Subscribe x) where
   subscribe func observer = func observer 
+
+instance Observable a ([a]) where
+  subscribe list observer = do
+    mapM observer list 
+    return (return ())
